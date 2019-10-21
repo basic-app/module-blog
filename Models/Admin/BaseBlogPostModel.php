7bc540d8dfedcc9450e169a29f1e370584035c77
basic-app <dev@basic-app.com>
2019-10-21 23:25:45 +0300

    n/a

--- Models/Admin/BaseBlogPostModel.php
+++ Models/Admin/BaseBlogPostModel.php
@@ -24,7 +24,7 @@ abstract class BaseBlogPostModel extends \BasicApp\Blog\Models\BlogPostModel
 	protected $validationRules = [
 		'post_title' => 'trim|max_length[255]|required',
 		'post_description' => 'trim|max_length[255]|required',
-		'post_slug' => 'trim|max_length[255]|required|alpha_dash|is_unique[blog_posts.post_slug,post_id,{post_id}]',
+		'post_slug' => 'trim|max_length[255]|required|alpha_dash|is_unique[posts.post_slug,post_id,{post_id}]',
 		'post_text' => 'trim|max_length[65535]',
 		'post_active' => 'in_list[0,1]',
         'post_lang' => 'max_length[2]'
