ca8195c135d888084f1d74fbdf53de748968d5bf
basic-app <dev@basic-app.com>
2020-02-18 21:36:19 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,12 +1,12 @@
 <?php
 
-$routes->add('blog', 'BasicApp\Blog\Controllers\BlogPost::index');
-$routes->add('blog/(:num)-(:segment)', 'BasicApp\Blog\Controllers\BlogPost::view/$1');
-$routes->add('blog/(:num)', 'BasicApp\Blog\Controllers\BlogPost::view/$1');
+$routes->add('blog', '\BasicApp\Blog\Controllers\BlogPost::index');
+$routes->add('blog/(:num)-(:segment)', '\BasicApp\Blog\Controllers\BlogPost::view/$1');
+$routes->add('blog/(:num)', '\BasicApp\Blog\Controllers\BlogPost::view/$1');
 
-$routes->get('{locale}/blog', 'BasicApp\Blog\Controllers\BlogPost::index');
-$routes->add('{locale}/blog/(:num)-(:segment)', 'BasicApp\Blog\Controllers\BlogPost::view/$1');
-$routes->add('{locale}/blog/(:num)', 'BasicApp\Blog\Controllers\BlogPost::view/$1');
+$routes->get('{locale}/blog', '\BasicApp\Blog\Controllers\BlogPost::index');
+$routes->add('{locale}/blog/(:num)-(:segment)', '\BasicApp\Blog\Controllers\BlogPost::view/$1');
+$routes->add('{locale}/blog/(:num)', '\BasicApp\Blog\Controllers\BlogPost::view/$1');
 
-$routes->add('admin/blog-post', 'BasicApp\Blog\Controllers\Admin\BlogPost::index');
-$routes->add('admin/blog-post/(:segment)', 'BasicApp\Blog\Controllers\Admin\BlogPost::$1');
+$routes->add('admin/blog-post', '\BasicApp\Blog\Controllers\Admin\BlogPost::index');
+$routes->add('admin/blog-post/(:segment)', '\BasicApp\Blog\Controllers\Admin\BlogPost::$1');
