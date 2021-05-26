0f9422a48052276792bd223176dd0dc7069ecdee
basic-app <dev@basic-app.com>
2021-05-26 16:50:19 +0300

    n/a

--- Database/Migrations/20190119024031_create_posts_table.php
+++ Database/Migrations/20190119024031_create_posts_table.php
@@ -6,7 +6,7 @@
  */
 namespace BasicApp\Blog\Database\Migrations;
 
-class Migration_create_posts_table extends \BasicApp\Core\Migration
+class Migration_create_posts_table extends \BasicApp\Migration\BaseMigration
 {
 
 	public $tableName = 'posts';
